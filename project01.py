b504305aec1ab7c1f7c174c5f79cdfac86c9e2df
arif <moarifur@gmail.com>
2022-06-23 21:00:44 +0600

    Project01: Band Name Generator

---
+++ project01.py
@@ -0,0 +1,17 @@
+"""
+------------------------------------------------------------------------
+Day01: Working with Variables in Python to Manage Data[Beginner]
+Project01: Band Name Generator
+Description: Take two input from user and combine to get a new string
+Steps:
+    - Variable in python
+    - Input in python
+    - String manipulation
+-------------------------------------------------------------------------
+"""
+
+print('Welcome to the Band Name Generator')
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print(f'Your band name could be {city_name} {pet_name}')
+
