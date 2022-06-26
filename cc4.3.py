2cbd84811c52a9f5cde77a57e6bfceafa52c805c
arif <moarifur@gmail.com>
2022-06-26 11:59:26 +0600

    Some changes in cc4.3.py

--- cc4.3.py
+++ cc4.3.py
@@ -8,6 +8,33 @@ Things to learn:
     - String reverse by slice notation [::-1]
 ---------------------------------------------------------------------------
 """
+# row1 = ['⬜️', '⬜️', '⬜️']
+# row2 = ['⬜️', '⬜️', '⬜️']
+# row3 = ['⬜️', '⬜️', '⬜️']
+# treasure_map = [row1, row2, row3]
+# print(f'{row1}\n{row2}\n{row3}\n')
+#
+# position = input('Where do you wanna put the treasure?\n')
+# new_position = position[::-1]
+# rc_pos = list(new_position)
+# treasure_map[int(rc_pos[0])-1][int(rc_pos[1])-1] = 'X'
+#
+# print(f'{row1}\n{row2}\n{row3}\n')
+#
+# # print(rc_pos)
+# # print(treasure_map[int(rc_pos[0])-1][int(rc_pos[1])-1])
+
+
+"""
+------------------------ Alternative Way ---------------------------------
+Day04: Randomisation and Python Lists[Beginner]
+Code Challenge 4.2: Treasure Map
+Things to learn:
+    - Variable, input()
+    - List Method: list() - to convert string to list
+    - String reverse by slice notation [::-1]
+---------------------------------------------------------------------------
+"""
 row1 = ['⬜️', '⬜️', '⬜️']
 row2 = ['⬜️', '⬜️', '⬜️']
 row3 = ['⬜️', '⬜️', '⬜️']
@@ -15,11 +42,10 @@ treasure_map = [row1, row2, row3]
 print(f'{row1}\n{row2}\n{row3}\n')
 
 position = input('Where do you wanna put the treasure?\n')
-new_position = position[::-1]
-
-rc_pos = list(new_position)
-treasure_map[int(rc_pos[0])-1][int(rc_pos[1])-1] = 'X'
-# print(rc_pos)
-# print(treasure_map[int(rc_pos[0])-1][int(rc_pos[1])-1])
+horizontal = int(position[1])
+vertical = int(position[0])
+treasure_map[horizontal-1][vertical-1] = 'X'
 
 print(f'{row1}\n{row2}\n{row3}\n')
+
+# print(horizontal, vertical)
